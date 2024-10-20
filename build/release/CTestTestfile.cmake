3e0b14357f050667696521545d9675e1a3471868
typeAl_ <161527750+typeAl@users.noreply.github.com>
2024-10-20 02:16:05 -0400

    fix: github workflows

---
+++ build/release/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tand/CLion/nightglow-lang
+# Build directory: /Users/tand/CLion/nightglow-lang/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cli")
+subdirs("lang")
+subdirs("tests")
---
+++ build/release/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tand/CLion/nightglow-lang/tests
+# Build directory: /Users/tand/CLion/nightglow-lang/build/release/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[nightglow-tests]=] "/Users/tand/CLion/nightglow-lang/build/release/Release/bin/nightglow-tests")
+set_tests_properties([=[nightglow-tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/tand/CLion/nightglow-lang/tests/CMakeLists.txt;11;add_test;/Users/tand/CLion/nightglow-lang/tests/CMakeLists.txt;0;")
